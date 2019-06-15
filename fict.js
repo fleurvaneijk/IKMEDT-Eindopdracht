9ab7461b3860062ee647a21d2c6a928696e2e066
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-06-15 18:04:58 +0200

    fict challenge start

---
+++ fict.js
@@ -0,0 +1,47 @@
+// var aframe = require('aframe');
+// var lookAt = require('aframe-look-at-component');
+
+window.onload = () => {
+  var sceneEl = document.getElementById('js--scene');
+
+  var hacker = document.createElement('a-image');
+  hacker.setAttribute('src', 'images/hacker.png');
+  hacker.setAttribute('position', '-1 2 -5');
+  // setPosition(hacker);
+  hacker.setAttribute('height', '1');
+  hacker.setAttribute('width', '1');
+  sceneEl.appendChild(hacker);
+
+  for(var i = 0 ; i < 100 ; i++) {
+
+    var imageNr = Math.floor(Math.random() * 7) + 1;
+    var path = 'images/person_' + imageNr + '.png';
+
+    var entityEl = document.createElement('a-image');
+    entityEl.setAttribute('src', path);
+
+    setPosition(entityEl);
+
+    entityEl.setAttribute('look-at', '[camera]');
+    entityEl.setAttribute('height', '1');
+    entityEl.setAttribute('width', '1');
+
+    sceneEl.appendChild(entityEl);
+  }
+}
+
+setPosition = (entity) => {
+  var numX = Math.floor(Math.random()*10) + 1;
+  numX *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+  var myX = numX;
+
+  var numY = Math.floor(Math.random()*7) + 1;
+  numY *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+  var myY = numY;
+
+  var numZ = Math.floor(Math.random()*10);
+  numZ *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+  var myZ = numZ;
+
+  entity .setAttribute('position', {x: myX, y: myY, z: myZ});
+}
--- main.js
+++ main.js
@@ -12,34 +12,3 @@ enterChallenge = (event) => {
     location.replace('fict.html');
   }
 }
-
-
-// pickUp = (event) => {
-//   let color = event.target.getAttribute("color");
-//   camera.appendChild(makeBox(color,"-0.1 0 -0.3", "0.1"))
-// }
-//
-// placeObject = (event) => {
-//   scene.appendChild(makeBox(
-//       document.getElementById("js--camera-box").getAttribute("color"),
-//       "3 0.25 0", "0.5"
-//     )
-//   )
-// }
-//
-//
-// makeBox = (color, position, space) => {
-//
-//   if(document.getElementById("js--camera-box")) {
-//     document.getElementById("js--camera-box").remove();
-//   }
-//
-//   let box = document.createElement("a-box");
-//   box.setAttribute("id", "js--camera-box");
-//   box.setAttribute("color", color);
-//   box.setAttribute("position", position);
-//   box.setAttribute("width", space);
-//   box.setAttribute("height", space);
-//   box.setAttribute("depth", space);
-//   return box;
-// }
