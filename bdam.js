18f4f5a9097a04d9fd9cb2f7a575ed62c1fe5af8
Unknown <stanhoenson@icloud.com>
2019-06-18 17:25:59 +0200

    things

--- bdam.js
+++ bdam.js
@@ -9,12 +9,13 @@ var counter3 = 0;
 
 pickUp = (event) => {
   let color = event.target.getAttribute("color");
+  document.getElementById(event.target.getAttribute("id")).remove();
   camera.appendChild(makeBox('js--camera-box', color,"-0.2 0 -0.4", "0.075"))
 }
 
 placeObject = (event) => {
-
   var planePosition = event.target.getAttribute('position');
+  console.log("planePosition: " + planePosition);
 
   if(event.target.getAttribute('color') != document.getElementById("js--camera-box").getAttribute("color")) {
     return;
@@ -30,15 +31,20 @@ placeObject = (event) => {
 
   console.log(counter1, counter2, counter3);
 
-  scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),planePosition, "0.5"));
+  // if(counter1 == 0) {
+  //   scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),-2 0.126 -6, "0.5"));
+  // }
 
+  scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),planePosition, "0.5"));
   document.getElementById("js--camera-box").remove();
+
+  leaveChallenge()
 }
 
 
 makeBox = (id, color, position, space) => {
-
   let box = document.createElement("a-box");
+
   box.setAttribute("id", id);
   box.setAttribute("color", color);
   box.setAttribute("position", position);
@@ -47,4 +53,10 @@ makeBox = (id, color, position, space) => {
   box.setAttribute("depth", space);
 
   return box;
+}
+
+leaveChallenge = (event) => {
+  if(counter1 == 1 && counter2 == 2 && counter3 == 3) {
+    location.replace('index.html');
+  }
 }
