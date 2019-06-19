6cdeffc671108244c0004d9a7052e81a40b7a4b7
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-06-19 15:51:04 +0200

    sky

--- bdam.js
+++ bdam.js
@@ -23,30 +23,28 @@ placeObject = function(event) {
 
   if(event.target.getAttribute("color") == colors[0]){
     if(counter1 == 0) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"-2 0.626 -6", "1"));
-      console.log("if");
+      scene.appendChild(makeBox("1box1", document.getElementById("js--camera-box").getAttribute("color"),"-2 0.626 -6", "1"));
     }
     counter1++;
-  } else if(event.target.getAttribute("color") == colors[1]){
+  }
+  else if(event.target.getAttribute("color") == colors[1]){
     if(counter2 == 0) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"0 0.626 -6", "1"));
+      scene.appendChild(makeBox("2box1", document.getElementById("js--camera-box").getAttribute("color"),"0 0.626 -6", "1"));
     } else if(counter2 == 1) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"0 1.626 -6", "1"));
+      scene.appendChild(makeBox("2box2", document.getElementById("js--camera-box").getAttribute("color"),"0 1.626 -6", "1"));
     }
     counter2++;
-  } else if(event.target.getAttribute("color") == colors[2]){
+  }
+  else if(event.target.getAttribute("color") == colors[2]){
     if(counter3 == 0) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"2 0.626 -6", "1"));
+      scene.appendChild(makeBox("3box1", document.getElementById("js--camera-box").getAttribute("color"),"2 0.626 -6", "1"));
     } else if(counter3 == 1) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"2 1.626 -6", "1"));
+      scene.appendChild(makeBox("3box2", document.getElementById("js--camera-box").getAttribute("color"),"2 1.626 -6", "1"));
     } else if(counter3 == 2) {
-      scene.appendChild(makeBox("test", document.getElementById("js--camera-box").getAttribute("color"),"2 2.626 -6", "1"));
+      scene.appendChild(makeBox("3box3", document.getElementById("js--camera-box").getAttribute("color"),"2 2.626 -6", "1"));
     }
     counter3++;
   }
-
-  console.log(counter1, counter2, counter3);
-
   document.getElementById("js--camera-box").remove();
 
   leaveChallenge()
