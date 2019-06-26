709330d854fc9285a2f4c43e32d0eeacdfc1f31f
Fleur <fleur.vaneijk99@gmail.com>
2019-06-26 17:44:55 +0200

    se and iat gone from hub

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 enterChallenge = function(event) {
   if (event.target.id == "js--bdam") {
     location.replace("bdam.html");
-  } else if (event.target.id == "js--se") {
-    location.replace("se.html");
-  } else if (event.target.id == "js--iat") {
-    location.replace("iat.html");
-  } else if (event.target.id == "js--fict") {
+  }
+  // else if (event.target.id == "js--se") {
+  //   location.replace("se.html");
+  // }
+  // else if (event.target.id == "js--iat") {
+  //   location.replace("iat.html");
+  // }
+  else if (event.target.id == "js--fict") {
     location.replace("fict.html");
   }
 };
